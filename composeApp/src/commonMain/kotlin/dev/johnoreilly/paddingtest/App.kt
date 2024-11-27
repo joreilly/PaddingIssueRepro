b93308016c79f6e24322b2195488a6045205747e
John O'Reilly <oreillyj@gmail.com>
2024-11-27 08:56:19 +0000

    add bottom bar + dummy content

--- composeApp/src/commonMain/kotlin/dev/johnoreilly/paddingtest/App.kt
+++ composeApp/src/commonMain/kotlin/dev/johnoreilly/paddingtest/App.kt
@@ -2,11 +2,18 @@ package dev.johnoreilly.paddingtest
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import org.jetbrains.compose.ui.tooling.preview.Preview
@@ -16,24 +23,45 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Preview
 fun App() {
     MaterialTheme {
-        Scaffold(
-            // have bottom bar here for example but same behaviour
-            // regardless
-        ) {
+        Scaffold(bottomBar = { BottomBar() }) {
+
             // this padding here seems to cause the issue
             Column(Modifier.padding(it)) {
 
                 // "nested" Scaffold with top app bar
-                // (normally in some nexted composable)
+                // (normally in some nested composable)
                 Scaffold(
-                    topBar = { TopAppBar(title = { Text("Some title") }) }
+                    topBar = { CenterAlignedTopAppBar(
+                        title = { Text("Some title") }
+                    )}
                 ) {
                     Column(Modifier.padding(it)) {
-                        Text("Hi")
-                        Text("there")
+                        LazyColumn {
+                            items(50) { index ->
+                                Text(text = "Item: ${index+1}")
+                            }
+                        }
                     }
                 }
             }
         }
+    }
+}
+
+@Composable
+private fun BottomBar() {
+    NavigationBar{
+        NavigationBarItem(
+            selected = false,
+            onClick = { },
+            icon = { Icon(imageVector = Icons.Filled.LocationOn, contentDescription = "",) },
+            label = { Text("Option 1") },
+        )
+        NavigationBarItem(
+            selected = false,
+            onClick = { },
+            icon = { Icon(imageVector = Icons.Filled.Person, contentDescription = "",) },
+            label = { Text("Option 2") },
+        )
     }
 }
